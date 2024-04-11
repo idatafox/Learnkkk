8191f70da4330133fdc397d316cd52478cdd040b
idatafox <wishlong@yahoo.com>
2024-04-11 14:41:34 +0800

    更新 index_lk.php

--- index_lk.php
+++ index_lk.php
@@ -1,1 +1,21 @@
-hello
+function initTit(){
+	  	   htmlcc="";
+       cc={topic:"root"};
+       $.ajax({	
+
+        type: "POST",
+
+        dataType: "text",
+        url: "/images_manager/imgapp_listprojects.php",
+        data:cc,
+        success: function(result){
+	
+         },//success over
+         complete:function(data){
+	
+         }// complete over
+       });//ajax over
+
+       tit="走遍中国";
+}
+
