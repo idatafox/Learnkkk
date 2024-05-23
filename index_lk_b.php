634b312028883a8347ba89b6c72a11064fe8c128
idatafox <wishlong@yahoo.com>
2024-05-23 10:11:45 +0800

    更新

---
+++ index_lk_b.php
@@ -0,0 +1,296 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>财经点读</title>
+    <link href="/bootstrap5/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+ <link href="/css/root.css?x=10" rel="stylesheet" >
+ 
+ <link rel="stylesheet" href="/jqueryUI011302/jquery-ui.css" >
+ <script src="/js/jquery.js"></script>
+   
+  <!--
+  <script src="https://www.elearn007.com/js/gl.js"></script>
+  -->
+  <script src="/js/imgapp.js?x=151"></script>
+  <script src="/jqueryUI011302/jquery-ui.js"></script>
+  </head>
+  <body style="padding-left:5px;padding-right:5px; background:white" >
+  	
+  
+
+  <script>
+  
+  $(document).ready(function(){
+	      //get global variables :tit
+       
+        initTit("fa");
+       // createVoiceWall("少儿英语");
+       // addLisenToCarousel();
+        ifdebug="0";
+        //getprojectsByArray("root");
+        $("#logfull").val(txt);
+        
+        
+        
+        
+        
+   });//doc
+   
+  </script>
+  
+  
+  <ul class="nav">
+	    <li class="nav-item">
+
+    <a class="nav-link" href="#"></a>
+
+    </li>
+  <li class="nav-item">
+	   
+    <a class="nav-link active" aria-current="page" href="#">
+	
+		    <img src="/images/youdian.jpg" width="100px" height="60px"/>
+		    财经新闻
+    </a>
+  </li>
+  <!--
+  <li class="nav-item">
+    <a class="nav-link" href="/index_code.php">编程英语</a>
+  </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#">走遍中国</a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link " href="/images_manager/reportlist.php">AI自习室</a>
+  </li>-->
+  
+</ul>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  <!-- 图片轮播  -->
+  <div id="carouselExampleCaptions" class="carousel slide">
+  <div id="indicators" class="carousel-indicators">
+
+  </div>
+  <!--要挂一个语音墙 -->
+  <div id="inner" class="carousel-inner">
+	
+
+
+  
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<!--语音墙结束位置 -->
+
+
+
+ <div class="container 
+ text-center gfont">
+    <!--  图片墙动态生成.开始-->
+ <div id="bbb"></div>
+
+    
+    <!--  图片墙动态生成.结束-->
+    
+
+
+
+                
+
+     
+          <h1 class="paddingCfg">
+          优点第一期：学校场景
+         <h1>
+	<!-- 
+   display  english
+   
+  -->
+  
+	 <div id="ivlist" class="container 
+
+ text-center gfont">
+
+         
+     
+ </div><!--box end-->
+  
+  
+  
+  
+  
+  
+  
+
+         <p class="">
+         	     <ul class="fc">
+         	          <li>
+                                <button type="button" class="btn btn-danger">我的服务</button>
+                        </li>
+                          <li>
+	          
+	   <ul class="fc">
+	            <li>
+	                 <h2>  欢迎扫码加好友  </h2>
+	           </li>
+	           <li>
+	            <img   src="/images/06.png?x=9" class="img-fluid"/>
+                                        </li>
+                                         <li>
+                                         </li>
+                                  </ul>
+                           </li>
+         	     </ul>
+     	</p>
+        
+
+    
+    <script src="/bootstrap5/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+   
+   
+   
+   
+   <div class="container text-center gfont">
+   
+     
+     
+  <div class="row">
+    <div class="col-10 " >
+        
+        <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true" >
+
+
+
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 id="arc" class="modal-title" >Modal title00</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+            <p id="title_doc"> </p>
+            <imglist id="ilist">
+	
+	                  <imgItem orderpos="0">
+		                     <div class="imgenty">
+			                      <img src=""/>
+		                     </div>
+		                     
+			                        <p class="imgdoc">
+				
+			                        </p>
+		                     
+			                </imgItem>
+			                
+	           </imglist>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+        
+        
+        
+    </div><!--  col end -->
+    <div class="col-1 ">
+	   </div>
+		 </div>
+		 
+		 
+
+		 
+		 
+		 
+
+</div>
+
+
+
+
+
+
+	
+
+
+
+
+        <!--   foot    -->
+
+	<p></p>
+
+	<p></p>
+    <p></p>
+	
+	<div class="container bkA">
+		
+		<div class="row ">
+    <div class="col-3" >
+           
+    </div>
+    <div class="col-6">
+    	        <p></p>
+           <img   src="/images/06.png" class="img-fluid"/>
+        </div>
+        
+         <div class="col-3" >
+           
+        </div>
+     
+    </div>
+		
+		
+		
+		
+		<div class="row paddingCfg-a">
+    <div class="col-1 " >
+           
+    </div>
+    <div class="col-11">
+           <p class="paddingCfg-a">
+             <a  href="https://beian.miit.gov.cn/" style="color:white;"> 
+       
+             </a>
+          </p>
+        </div>
+     
+    </div>
+   
+    </div>
+      <script src="/bootstrap5/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
+    </body>
+  </html>
+	<!--
+	<button onclick="test1()">测试演示</button>
+		<textarea id="logfull" rows="200"
+
+	          cols="300">
+
+		
+	</textarea>
+	-->
+	
